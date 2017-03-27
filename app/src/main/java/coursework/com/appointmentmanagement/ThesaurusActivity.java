bb1c676bb32c85fdbfc66baa779c217d084823ef
Mewantha <mewa.bandara@gmail.com>
2017-03-27 06:47:07 +0530

    Added ListView to thesaurus

--- app/src/main/java/coursework/com/appointmentmanagement/ThesaurusActivity.java
+++ app/src/main/java/coursework/com/appointmentmanagement/ThesaurusActivity.java
@@ -4,16 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -26,14 +35,15 @@ public class ThesaurusActivity extends Activity {
     private Request request;
     TextView tv1;
     Response response;
-
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+    private ArrayList<String> logData = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thesaurus);
         Intent intent = getIntent();
-        tv1=(TextView)findViewById(R.id.textView1);
-
+        //tv1 = (TextView) findViewById(R.id.textView1);
         String wordToSend = intent.getStringExtra("word");
         String url = "http://thesaurus.altervista.org/thesaurus/v1?word="+wordToSend+"&language=en_US& key=AtwIFkvWfU2rdYlngsoi&output=xml";
         client = new OkHttpClient();
@@ -72,18 +82,41 @@ public class ThesaurusActivity extends Activity {
             element.normalize();
 
             NodeList nList = doc.getElementsByTagName("list");
-
+            int indexOfMainArray = 0;
             for (int i=0; i<nList.getLength(); i++) {
 
                 Node node = nList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element element2 = (Element) node;
-                    tv1.setText(tv1.getText()+"\n    " + getValue("synonyms", element2)+"\n");
-                    tv1.setText(tv1.getText()+"-----------------------");
+
+                    String dateFromInputStreamString =  getValue("synonyms", element2);
+                    dateFromInputStreamString = dateFromInputStreamString.replace('|',',');
+                    String[] dateFromInputStream = dateFromInputStreamString.split(",");
+
+                    System.out.print("");
+                    for (String aDateFromInputStream : dateFromInputStream) {
+                        Log.d(TAG, "qerwer " + aDateFromInputStream);
+
+                        logData.add(aDateFromInputStream);
+                        //tv1.setText(tv1.getText()+"\n    " + aDateFromInputStream+"\n");
+                        indexOfMainArray++;
+
+                    }
+
                 }
             }
+            mainListView = (ListView) findViewById( R.id.mainListView );
 
-        } catch (Exception e) {e.printStackTrace();}
+            // Create ArrayAdapter using the planet list.
+            listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, logData);
+            mainListView.setAdapter( listAdapter );
+        }catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
